5429d058a25ffd818826f88303a0bcfd8e237c36
Maria Rotolo <mariarotolo1994@gmail.com>
2021-12-14 16:56:05 +0100

    esercizio terminato

--- src/components/AddVat/index.jsx
+++ src/components/AddVat/index.jsx
@@ -1,2 +1,14 @@
-<AddVAT value={100}
- vat={22} />
+
+const AddVAT = (props) => {
+ 
+ const tot = (props.value * props.vat / 100) + props.value;
+ return (
+     <div>
+         <p>Product: {props.value}</p>
+         <p>VAT: {props.vat}%</p>
+         <p>Tot: {tot}</p>
+     </div>
+ )
+}
+
+export default AddVAT;
